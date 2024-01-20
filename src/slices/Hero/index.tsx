99ab0ad0ceb26e04bf2ebdd4517a3c8b0aaa31f1
NicklausDev <nicklauswandera@gmail.com>
2024-01-20 04:02:48 +0300

    Initialized animation of symbols

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -4,7 +4,9 @@ import { useEffect, useRef } from "react";
 
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import gsap from "gsap";
+import { gsap } from "gsap";
+import Bounded from "@/components/Bounded";
+import Shapes from "./Shapes"
 
 /**
  * Props for `Hero`.
@@ -74,13 +76,14 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
 
 
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={component}
     >
 
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <Shapes />
         <div className="col-start-1 md:row-start-1">
         <h1 
           className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" 
@@ -100,7 +103,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           
         </div>
       </div>
-    </section>
+    </Bounded>
   );
 };
 
